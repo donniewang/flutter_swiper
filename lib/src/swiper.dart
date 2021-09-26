d8d7472581e26a9ca85e88019adbd54e2d2a17cf
donniewang <DonnieWang@qq.com>
2021-09-26 16:06:05 +0800

    Update swiper.dart

--- lib/src/swiper.dart
+++ lib/src/swiper.dart
@@ -861,7 +861,7 @@ class _StackViewState extends _CustomLayoutStateBase<_StackSwiper> {
 
   void _updateValues() {
     if (widget.scrollDirection == Axis.horizontal) {
-      double space = (_swiperWidth - widget.itemWidth) / 2;
+      double space = (_swiperWidth - widget.itemWidth) / 2 + 10;
       offsets = [-space, -space / 3 * 2, -space / 3, 0.0, _swiperWidth];
     } else {
       double space = (_swiperHeight - widget.itemHeight) / 2;
